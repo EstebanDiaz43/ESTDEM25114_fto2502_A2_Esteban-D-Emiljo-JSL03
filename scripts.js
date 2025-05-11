491cabd642f51ad723ccffd8aa39e6a7268f7bd7
EstebanDiaz43 <esteban.demiljo@gmail.com>
2025-05-11 12:34:11 +0200

    Added comments

--- scripts.js
+++ scripts.js
@@ -74,10 +74,12 @@ const initialTasks = [
   },
 ];
 
+// Function to add tasks
 function addTasks(TaskArray, maxNewTasks = 3) {
   let AddedTasks = 0;
   let lastId = TaskArray[TaskArray.length - 1]?.id || 0;
 
+  // Loop to add new tasks
   while (AddedTasks < maxNewTasks) {
     const addAnother = confirm("Add a new task?");
     if (!addAnother) break;
@@ -86,6 +88,7 @@ function addTasks(TaskArray, maxNewTasks = 3) {
     const description = prompt("Enter task description:");
     const status = prompt("Enter task status (todo, doing, done):");
 
+    // Validate status
     lastId++;
     const NewTask = {
       id: lastId,
@@ -98,6 +101,7 @@ function addTasks(TaskArray, maxNewTasks = 3) {
     AddedTasks++;
   }
 
+  // Check if the maximum number of new tasks has been reached
   if (AddedTasks === maxNewTasks) {
     alert(
       "There are enough tasks on your board, please check them in the console."
@@ -105,10 +109,12 @@ function addTasks(TaskArray, maxNewTasks = 3) {
   }
 }
 
+// Function to filter completed tasks
 function filterCompletedTasks(taskArray) {
   return taskArray.filter((task) => task.status === "done");
 }
 
+// Function to log all tasks and completed tasks
 function logTasks(taskArray) {
   console.log("All Tasks");
   console.log(taskArray);
